a70c782af38579b9448bed8c0962cd12a8ddb042
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2019-03-12 10:57:07 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Toggle from './Toggle';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  
+ const [name, setName] = useTitleInput('');
+  
+  
+  return (
+    <div className="main-wrapper">
+      <h1>Level Up Dishes</h1>
+      <Toggle />
+      <h3>{name}</h3>
+      
+      <form onSubmit={
+        e => {
+          e.preventDefault();
+         
+        }
+      }>
+        <input 
+          type="text" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+          />
+          <button>Submit</button>
+      </form>
+    
+    </div>
+  )
 }
+
+function useTitleInput(initalValue){
+  const [value, setValue] = useState(initalValue);
+
+  useEffect(() => {
+    document.title = value;
+
+  });
+  return [value, setValue]
+
+}
+
+// const formSubmit = (value, setValue) => {
+//   console.log('email sent to ' + value + "!");
+//   setValue("");
+// }
 
 export default App;
---
+++ src/Toggle.js
@@ -0,0 +1,42 @@
+import React, { useState} from 'react';
+
+const Toggle = () => {
+
+    const [isToggled, setToggled] = useState(false)
+
+    return (
+        <div>
+            <button onClick={() => setToggled(!isToggled)}>Toggle</button>
+            { isToggled && <h2>Hello</h2>}
+        </div>
+    )
+}
+
+
+export default Toggle;
+
+// import React, {Component} from "react";
+
+// export default class Toggle extends Component {
+//     state = {
+//         isToggled: false
+//     };
+
+//     toggle = () => {
+//         this.setState(state => {
+//             return {
+//                 isToggled: !state.isToggled
+//             };
+//         });
+//     };
+
+//     render() {
+//         return ( <div>
+//             <button onClick = {
+//                 this.toggle
+//             } > Toggle </button> {
+//                 this.state.isToggled && <h2> Hello! </h2>} </div>
+//             );
+//         }
+//     }
+  
